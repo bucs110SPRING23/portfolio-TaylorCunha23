27f4657647e3a8e656471e16b56b3ca2a40b8b22
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-13 22:51:29 +0000

    :)

--- ch02/notes.py
+++ ch02/notes.py
@@ -6,3 +6,12 @@ import math
 mylist = [1,2,3,4,5,6,7,8,9,10]
 var = random.choice(mylist)
 print(var)
+
+# For loop: repeats a sequential algorithm on sequential data
+# For loops are the only area you can use single letters
+
+# Block statement always ends with a colon and everything part of the block is idented.
+
+mylist = [1,2,3,4,5]
+for i in mylist:
+    print(i+i)
