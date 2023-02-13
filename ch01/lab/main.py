31103b317c877bcd9c98f194725062aa985dda49
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-13 22:57:44 +0000

    :)

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -5,12 +5,12 @@ weeks = 16
 classes = 5
 tuition = 6000
 cost_per_week = ((tuition / classes) / weeks)
-print("Cost per week:", cost_per_week, type(cost_per_week))
+print("Cost per week:", "$", cost_per_week, type(cost_per_week))
 
 classes_per_week = 3
 cost_per_class = cost_per_week / classes_per_week
 var = cost_per_class
-print("Cost per class:", var, type(cost_per_class))
+print("Cost per class:", "$", cost_per_class, type(cost_per_class))
 
 #Part B
 Months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ]
