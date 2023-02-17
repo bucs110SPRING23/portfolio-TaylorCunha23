f0f79199c843648e4e96ba6ab31d5b5ed04da4a2
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-17 23:46:30 +0000

    :)

---
+++ ch02/exercises/drawing.py
@@ -0,0 +1,18 @@
+import turtle             
+my_window = turtle.Screen() 
+my_window.bgcolor("blue")      
+my_pen = turtle.Turtle()      
+my_pen.forward(150)           
+my_pen.left(90)               
+my_pen.forward(75)
+my_pen.color("white")
+my_pen.pensize(12)
+
+sides = int(input("Enter the number of sides of your shape:"))
+print = sides
+var1 = sides
+
+length = int(input("Enter the length of each side:"))
+print = length
+var2= length
+
