eef3bcfa4ea95f09ccf862d7dc097a640634de9e
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-20 23:02:43 +0000

    :)

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -1,12 +1,12 @@
 import turtle             
 my_window = turtle.Screen() 
-my_window.bgcolor("blue")      
-my_pen = turtle.Turtle()      
+my_window.bgcolor("light blue")      
+my_pen = turtle.Turtle()     
 my_pen.forward(150)           
 my_pen.left(90)               
 my_pen.forward(75)
 my_pen.color("white")
-my_pen.pensize(12)
+my_pen.pensize(1)
 
 sides = int(input("Enter the number of sides of your shape:"))
 print = sides
