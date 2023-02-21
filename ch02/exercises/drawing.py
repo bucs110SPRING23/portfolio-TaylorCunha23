9252ad7f259339bd2523c5f2f93d4fc063a9e499
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-21 01:21:54 +0000

    :)

--- ch02/exercises/drawing.py
+++ ch02/exercises/drawing.py
@@ -2,9 +2,6 @@ import turtle
 my_window = turtle.Screen() 
 my_window.bgcolor("light blue")      
 my_pen = turtle.Turtle()     
-my_pen.forward(150)           
-my_pen.left(90)               
-my_pen.forward(75)
 my_pen.color("white")
 my_pen.pensize(1)
 
@@ -16,3 +13,8 @@ length = int(input("Enter the length of each side:"))
 print = length
 var2= length
 
+for i in range(sides):
+    turtle.forward(length)
+    turtle.right(360 /sides)
+
+my_window.exitonclick()
