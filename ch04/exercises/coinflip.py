0815a92c36f80f6c90b027199d9fa6b619849488
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-27 21:34:07 +0000

    :)

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -1,2 +1,32 @@
 import turtle
 import random
+
+taylor = turtle.Turtle()
+taylor.color("brown")
+taylor.turtlesize(1)
+taylor.shape("turtle")
+turtle.Screen().bgcolor("light pink")
+
+Heads_or_Tails = [heads, tails]
+coinflip = random.choice(Heads_or_Tails)
+
+message = """
+        Flip a coin! 
+        Press enter.
+        """
+response = input(message)
+
+while taylor 
+for i in Heads_or_Tails:
+    if i = heads:
+        print("Heads!")
+        taylor.left(90)
+        taylor.foward(50)
+    else:
+        print("Tails!")
+        taylor.right(90)
+        taylor.foward(50)
+
+
+
+turtle.exitonclick()
