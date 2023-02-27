dad2adf79c6163cd9447ecf5d171d0e8f4b4217d
TaylorCunha23 <tcunha1@binghamton.edu>
2023-02-27 19:28:26 +0000

    :)

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -15,29 +15,24 @@ pygame.draw.line(screen, "black", (500,-1000), (500,1000), width=3)
 pygame.draw.line(screen, "black", (0,500), (1000,500), width=3)
 pygame.display.update()
 message = """
-        White darts hit the target. Red darts missed the target. Press enter.
+        10 darts will be randomly thrown. 
+        The darts that hit the target are in white. 
+        The darts that miss the target are in red. 
+        Press enter.
         """
 response = input(message)
 
 for darts in range(10):
-    x1 = random.randrange(0,1000)
-    y1 = random.randrange(0,1000)
-    x2 = random.randrange(0,1000)
-    y2 = random.randrange(0,1000)
-    distance_from_center1 = math.hypot(x1-500, y1-500)
-    distance_from_center2 = math.hypot(x2-500, y2-500)
-    is_in_circle1 = distance_from_center1 <= screen_width/2 #screen width
-    is_in_circle2 = distance_from_center2 <= screen_width/2
-    print(distance_from_center1, screen_width/2)
-    print(is_in_circle2)
-    if is_in_circle1:
-        pygame.draw.circle(screen, (255,255,255), [x1,y1], 10)
+    x = random.randrange(0,1000)
+    y = random.randrange(0,1000)
+    distance_from_center = math.hypot(x-500, y-500)
+    is_in_circle = distance_from_center <= screen_width/2 #screen width
+    print(distance_from_center, screen_width/2)
+    print(is_in_circle)
+    if is_in_circle:
+        pygame.draw.circle(screen, (255,255,255), [x,y], 10)
     else:
-        pygame.draw.circle(screen, (255, 0, 0), [x1,y1], 10) 
-    if is_in_circle2:
-        pygame.draw.circle(screen, (255,255,255), [x2,y2], 10)
-    else:
-        pygame.draw.circle(screen, (255, 0, 0), [x2,y2], 10) 
+        pygame.draw.circle(screen, (255, 0, 0), [x,y], 10) 
 pygame.display.flip()
 pygame.time.wait(10000)
 # screen.fill((153, 184, 152))
