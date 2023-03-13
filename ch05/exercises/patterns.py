89432b590b157c7c006ba1242435b01b844bd8e4
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-13 21:03:52 +0000

    :)

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,1 +1,10 @@
-star_pyramid()
+value = input("Enter the number of rows:")
+value = int(value)
+
+def star_pyramid(value):
+    myList = []
+    for i in range(1,value+1):
+        myList.append("*"*i)
+    print("\n".join(myList))
+
+star_pyramid(value)
