a0c96eaf4b7f914c384b2c58d05f017238ab72f4
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-20 21:35:43 +0000

    :)

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -25,23 +25,33 @@ def threenp1range(upper_limit):
 
 def graph_coordinates(threenplus1_iters_dict):
     upper_limit = 10
+    n = int(input("Positive integer:"))
+    count = threenp1(n)
     objs_in_sequence = threenp1range(upper_limit)
     threenplus1_iters_dict = dict.items(objs_in_sequence)
+    
+        
 
 def main():
     upper_limit = 10
     objs_in_sequence = threenp1range(upper_limit)
     n = int(input("Positive integer:"))
-    num_seq = []
+    #num_seq = []
     count = threenp1(n)
     print("Number you started with:", n)
     print("Iterations:", objs_in_sequence)
     print("Number of iterations:", count)
     threenplus1_iters_dict = dict.items(objs_in_sequence)
-    graph_coordinates(threenplus1_iters_dict)
-    x = objs_in_sequence
+    keys = objs_in_sequence.keys()
+    x = objs_in_sequence  
     y = count
-    pygame.draw.lines(screen, "black", dict(x), dict(y))
+   # pairs = zip(dict.values(x), dict.keys(y))
+    #points = []
+    #for x in objs_in_sequence:
+       # points.append(x)
+       # points.append(y)
+    #return points
+    graph_coordinates(threenplus1_iters_dict)
 
     width, height = new_display.get_size()
     new_display = pygame.transform.scale(new_display, (width * 5, height * 5))
