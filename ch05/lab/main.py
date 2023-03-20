497075f0ba2e8e1f91a1162ba5aa8dd0b4af3f7f
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-20 18:00:19 +0000

    :)

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,32 +1,39 @@
 import pygame
 pygame.init()
 
-
 def threenp1(n):
     count = 0
-    num_seq = []
-    if n < 1:
-       return []
     while n > 1:
         count = count + 1
         if n % 2 == 0:
             n = n / 2
-            count += 1
+            # count += 1
         else:
             n = 3 * n + 1
-            count += 1
-        num_seq.append(n)    
-    return num_seq
+            # count += 1    
     return count
 
 def threenp1range(upper_limit):
-    
+    upper_limit = 10
+    objs_in_sequence = {}
+    for i in range(2,upper_limit):
+        iters = threenp1(i)
+        objs_in_sequence[i] = iters
+    return objs_in_sequence
+
+def graph_coordinates(threenplus1_iters_dict):
+    dict.items()
+    pygame.draw.lines()
 
 def main():
+    upper_limit = 10
+    objs_in_sequence = threenp1range(upper_limit)
     n = int(input("Positive integer:"))
     num_seq = []
     count = threenp1(n)
     print("Number you started with:", n)
-    print("Iterations:", count)
-    print("Number of iterations:", len(count))
+    print("Iterations:", objs_in_sequence)
+    print("Number of iterations:", count)
+
+
 main()
