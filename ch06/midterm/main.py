2251ea2d981820b0c150de93738b05c0f5823a0d
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-20 16:49:01 +0000

    :)

--- ch06/midterm/main.py
+++ ch06/midterm/main.py
@@ -12,7 +12,7 @@ def eyes(eyes_color, radius):
     pen.circle(radius)
     pen.end_fill()
     pen.up()
- 
+
 def face(face_color,radius):
     pen.down()
     pen.fillcolor(face_color)
@@ -37,26 +37,33 @@ def mouth(mouth_color, radius, extent):
     pen.end_fill()
     pen.up()
 
-def main():
+def get_colors():
     print("Customize your smiley face!")
+    colors = {}
+    colors["face"] = input("What color is the face?:")
+    colors["eyes"] = input("What color are the eyes?:")
+    colors["nose"] = input("What color is the nose?:")
+    colors["mouth"] = input("What color is the mouth?:")
+    return colors
 
-    face_color = input("What color is the face?:")
-    face(face_color,100)
+
+def main():
+    colors = get_colors()
+
+    face(colors["face"],100)
     pen.goto(-40, 120)
 
-    eye_color = input("What color are the eyes?:")
-    eyes(eye_color, 15)
+    eyes(colors["eyes"], 15)
     pen.goto(-37, 125)
     pupils = "black"
     eyes(pupils, 5)
     pen.goto(40, 120)
-    eyes(eye_color, 15)
+    eyes(colors["eyes"], 15)
     pen.goto(40, 125)
     eyes(pupils, 5)
     pen.goto(0, 75)
 
-    nose_color = input("What color is the nose?:")
-    nose(nose_color, 8)
+    nose(colors["nose"], 8)
     pen.goto(-40, 85)
     pen.down()
     pen.right(90)
@@ -66,9 +73,9 @@ def main():
     pen.down()
     pen.right(180)
     
-    mouth_color = input("What color is the mouth?:")
-    mouth(mouth_color, 10, 180)
+    mouth(colors["mouth"], 10, 180)
     pen.hideturtle()
     turtle.done()
     window.exitonclick()
 main()
+
