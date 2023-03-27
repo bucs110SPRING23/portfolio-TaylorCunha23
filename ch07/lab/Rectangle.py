25c9cf95856692f6736e53eb6cf8a910c489cab8
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-27 21:29:36 +0000

    :)

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,18 +1,18 @@
 class Rectangle:
-    x = 0
-    y = 0
-    height = 20
-    width = 50
-    def __init__(self, x, y, h, w):
-        self.x = x
-        self.y = y
-        self.height = h
-        self.width = w
+    #x = 0
+    #y = 0
+    #height = 20
+    #width = 50
+    def __init__(self, x=0, y=0, h=20, w=50):
+        self.x = abs(x)
+        self.y = abs(y)
+        self.height = abs(h)
+        self.width = abs(w)
     def __str__(self):
         return f"The rectangle's x coordinate is {self.x}, y coordinate is {self.y}, height is {self.height}, and width is {self.width}"
-myRectangle = Rectangle(0, 0, 20, 50)
-print(myRectangle.__str__())
-print(myRectangle)
-print(str(myRectangle))
-print(myRectangle.__repr__())
+#myRectangle = Rectangle(0, 0, 20, 50)
+#print(myRectangle.__str__())
+#print(myRectangle)
+#print(str(myRectangle))
+#print(myRectangle.__repr__())
 
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -2,10 +2,10 @@ class Surface:
     def __init__(self, filename, Rectangle, x, y, h, w):
         self.image = filename
         self.rect = Rectangle
-        self.x = x
-        self.y = y
-        self.height = h
-        self.width = w
+        self.x = abs(x)
+        self.y = abs(y)
+        self.height = abs(h)
+        self.width = abs(w)
     def getRect(self):
         return self.rect
         r1 = Rectangle('name', 0, 0, 20, 50)
