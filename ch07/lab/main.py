fde18d4f1664f0d6d75d3ada57256f2b29cb7fb0
TaylorCunha23 <tcunha1@binghamton.edu>
2023-03-27 21:57:46 +0000

    :)

--- ch07/lab/main.py
+++ ch07/lab/main.py
@@ -14,7 +14,7 @@ def main():
     assert((r.x, r.y, r.height, r.width) == (1,1,1,1000))
     myRectangle = Rectangle(0, 0, 20, 50)
     s = Surface("myimage.png", myRectangle, 10, 10, 10, 10)
-    assert((s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
+    assert((s.rect.image, s.rect.rect, s.rect.x, s.rect.y, s.rect.height, s.rect.width) == (10,10,10,10))
     srect = s.getRect()
     assert(srect.x,  s.getRect().y, srect.height,  srect.width) == (10,10,10,10)
     assert s.image
