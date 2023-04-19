0d9c8fc2b780d5a07c9e855690aa589c41f4e56b
TaylorCunha23 <tcunha1@binghamton.edu>
2023-04-19 19:15:46 +0000

    :)

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -1,4 +1,5 @@
 vowel_list = ['a','e','i','o','u','A','E','I','O','U']
+string = ["interesting", "aardvark", "aaa", "aeiouAEIOU", "a b c d e f g h i j k l m n o p q r s t u v w x y z", '']
 
 class StringUtility:
     def __init__(self, string):
@@ -6,18 +7,16 @@ class StringUtility:
     def __str__(self):
         return f"{self.string}"
 
-    def vowels(string):
-        for char in string:
-            for vowel in vowel_list:
-                if char == vowel:
-                    count +=1
-                if vowels < 5:
-                    print(len(vowels))
-                if vowels >= 5:
-                    print("many")
+    def vowels(self):
+        count = 0
+        vowel = set("aeiouAEIOU")
+        for alphabet in string:
+            if alphabet in vowel:
+                count = count + 1
+        print("No. of vowels :", count)
 
-test_strings = ["interesting", "aardvark", "aaa", "aeiouAEIOU", "a b c d e f g h i j k l m n o p q r s t u v w x y z", '']
-string = test_strings
+
+
 
 
 
