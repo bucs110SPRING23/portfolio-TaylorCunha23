3e680e34601bedf5d03f85d898363ec21305ca6c
TaylorCunha23 <tcunha1@binghamton.edu>
2023-04-23 19:40:28 +0000

    :)

--- ch08/lab/stringutility.py
+++ ch08/lab/stringutility.py
@@ -7,13 +7,41 @@ class StringUtility:
     def __str__(self):
         return f"{self.string}"
 
+
+    # def vowels(self, vowel):
+    #     count = 0
+    #     self.vowel = vowel
+    #     for alphabet in string:
+    #         if alphabet in vowel:
+    #             count = count + 1
+    #     print("No. of vowels :", count)
+
     def vowels(self):
-        count = 0
-        vowel = set("aeiouAEIOU")
-        for alphabet in string:
-            if alphabet in vowel:
-                count = count + 1
-        print("No. of vowels :", count)
+        num_vowels=0
+        for char in self:
+            if char in "aeiouAEIOU":
+                num_vowels = num_vowels+1
+        if num_vowels < 5:
+            return str(num_vowels)
+        else:
+           return "many"
+        
+    
+    def bothEnds(self):
+        if len(self) < 3:
+            return ""
+        else:
+            return self[0] + self[1] + self[-2] + self[-1]
+
+    def fixStart(self):
+        if len(self) <=1:
+            return self
+        else:
+            first_char = self[0]
+            other_half = self[1:]
+            return first_char + other_half.replace(first_char, "*")
+  
+
 
 
 
