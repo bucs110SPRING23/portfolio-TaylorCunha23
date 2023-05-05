5472bb47d188cafc33adea7bd2b8d8c401d0c3cb
TaylorCunha23 <tcunha1@binghamton.edu>
2023-05-05 23:34:17 +0000

    :)

--- ch10/final_project/main.py
+++ ch10/final_project/main.py
@@ -1,16 +1,31 @@
-import pygame
+import requests
 
-# import your controller
+#Aries, Taurus, Gemini, Cancer, Leo, Virgo, Libra, Scorpio, Sagittarius, Capricorn, Aquarius, Pisces
+
+sign = input("Enter your zodiac sign:")
+
+day = input("Enter the horoscope day (today, tomorrow, or yesterday)")
+
+params = (
+
+    ('sign', sign),
+    ('day', day)
+
+)
+
+response = requests.post('https://aztro.sameerkumar.website/', params=params)
+
+print(response.json())
+
+# def main():
+#     pygame.init()
+#     # Create an instance on your controller object
+#     # Call your mainloop
+
+#     ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
 
 
-def main():
-    pygame.init()
-    # Create an instance on your controller object
-    # Call your mainloop
+# # https://codefather.tech/blog/if-name-main-python/
+# if __name__ == "__main__":
+#     main()
 
-    ###### NOTHING ELSE SHOULD GO IN main(), JUST THE ABOVE 3 LINES OF CODE ######
-
-
-# https://codefather.tech/blog/if-name-main-python/
-if __name__ == "__main__":
-    main()
